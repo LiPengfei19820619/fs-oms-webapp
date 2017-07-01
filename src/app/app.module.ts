aa2d8a00da912193bea285d2181bf405c4bcb30d
李鹏飞10039648 <li.pengfei2@zte.com.cn>
2017-07-01 19:51:56 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MdSidenavModule } from '@angular/material';
+import { MdButtonModule, MdIconModule } from '@angular/material';
+
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +13,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    MdSidenavModule,
+    MdButtonModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
